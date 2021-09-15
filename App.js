0ad78091700f98576e8c2bed47c4283df1421dc7
ayush <ayushmhjn12@gmail.com>
2021-09-15 19:22:50 +0530

    Test32

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const Section = ({children, title}): Node => {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        AM?PM
+        {title}
       </Text>
       <Text
         style={[
